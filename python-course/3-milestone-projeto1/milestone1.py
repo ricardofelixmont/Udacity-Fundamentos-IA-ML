b125d5dc263d41a0aadad17c40236d743b3ef6d8
Ricardo Felix <ricardofelix_mont@outlook.com>
2018-12-24 15:02:58 -0200

    Projeto finalizado

--- python-course/3-milestone-projeto1/milestone1.py
+++ python-course/3-milestone-projeto1/milestone1.py
@@ -14,33 +14,48 @@ with open('filmes.csv', 'r') as arquivo:
     for filme in dict_filmes:
         filmes.append(filme)
 
-# Fuções para manipular dos filmes
+# Fuções para manipular os filmes
+# ADICIONAR UM NOVO FILME
 def novo_filme(titulo, ano, diretor='desconhecido'):
     filmes.append({'titulo':titulo, 'ano':ano, 'diretor':diretor})
+
     with open('filmes.csv', 'a+') as filme:
-        fieldnames = ['titulo', 'ano', 'diretor']
+        fieldnames = ['titulo', 'ano', 'diretor'] # É um parametro obrigatorio do DictWriter(arquivo, fieldnames=fieldnames)  -> este é o cabecalho do arquivo csv
         writer = csv.DictWriter(filme, fieldnames=fieldnames)
         writer.writerow({'titulo': titulo, 'ano': ano, 'diretor':diretor})
+
     return 'Filme adicionado com sucesso...'
 
+# PROCURAR FILMES PELO ANO
 def procurar_filmes_ano(ano):
     for filme in filmes:
-        if filme['ano'] == ano:
+        # print(typ(filme['ano'])) # aqui vemos que tudo que vem do dicionario esta vindo como string
+        if int(filme['ano']) == ano:
             return filme
 
+# PROCURAR FILMES PELO TUTULO
 def procurar_filmes_titulo(titulo):
     for filme in filmes:
         if filme['titulo'] == titulo:
             return filme
+
+# PROCURAR FILMES PELO NOME DO DIRETOR
 def procurar_filmes_diretor(diretor):
     for filme in filmes:
         if filme['diretor'] == diretor:
             return filme
+
+# MOSTRAR TODOS OS FILMES
 def mostrar_todos():
     print('LISTA DE FILMES')
     for filme in filmes:
-        print(f'{"="*7} DADOS DO FILME {"="*7}\nTitulo: {filme["titulo"]}\nAno: {filme["ano"]}\nDiretor: {filme["diretor"]}\n{"="*15}')
-        
+        exibir_filme(filme)
+
+
+# FUNÇÃO PARA EXIBIR CADA FILME        
+def exibir_filme(filme):
+    print(f'{"="*7} DADOS DO FILME {"="*7}\nTitulo: {filme["titulo"]}\nAno: {filme["ano"]}\nDiretor: {filme["diretor"]}\n{"="*15}')
+
 
 # MENU
 print('-='*15)
@@ -63,23 +78,32 @@ while opcao not in [6]:
 
         novo_filme(titulo, ano, diretor)
         filme = procurar_filmes_titulo(titulo)
-        print(f'{"="*7} DADOS DO FILME {"="*7}\nTitulo: {filme["titulo"]}\nAno: {filme["ano"]}\nDiretor: {filme["diretor"]}\n{"="*15}')
+        exibir_filme(filme)
+
 
     elif opcao == 2:
         titulo = str(input('Digite o nome do filme: '))
         print(titulo)
         filme = procurar_filmes_titulo(titulo)
-        print(f'{"="*7} DADOS DO FILME {"="*7}\nTitulo: {filme["titulo"]}\nAno: {filme["ano"]}\nDiretor: {filme["diretor"]}\n{"="*15}')
+        exibir_filme(filme)
+
+
     elif opcao == 3:
         diretor = str(input('Digite o nome do Diretor: '))
 
         filme = procurar_filmes_diretor(diretor)
-        print(f'{"="*7} DADOS DO FILME {"="*7}\nTitulo: {filme["titulo"]}\nAno: {filme["ano"]}\nDiretor: {filme["diretor"]}\n{"="*15}')
+        exibir_filme(filme)
+
+
     elif opcao == 4:
         mostrar_todos()
+
+
     elif opcao == 5:
         ano = int(input('Digite o ano de lançamento: '))
         filme = procurar_filmes_ano(ano)
-        print(f'{"="*7} DADOS DO FILME {"="*7}\nTitulo: {filme["titulo"]}\nAno: {filme["ano"]}\nDiretor: {filme["diretor"]}\n{"="*15}')
+        exibir_filme(filme)
+
+
     else:
         print('Opção inválida, por favor tente novamente...')
